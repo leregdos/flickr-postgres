7d276096f4face7f46d1e17ee0d64fa87f8c99a4
Sonny Mandakhnaran <sm8694@nyu.edu>
2024-04-20 16:50:12 +0400

    Update app.py and rename directory

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from index import index
 from auth import user
+import psycopg2
 
 app = Flask(__name__)
+
+# Setup Database connection
+DB_HOST = "localhost"  # Database host
+DB_NAME = "flickr_postgres"  # Database name
+DB_USER = "username"  # Database username
+DB_PASS = "pass"  # Database password
+# Establish a connection to the PostgreSQL database with the above credentials
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
 # Register the Blueprints
 app.register_blueprint(index.index_blueprint, url_prefix='/')
